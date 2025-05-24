69fe2e349b2a8c5a026dcb88dd02ccd5e4ce1495
Hajavololona Armellin <armelgeek5@gmail.com>
2025-05-24 15:28:11 +0300

    feat: create user model and repository

---
+++ src/domain/repositories/user.repository.interface.ts
@@ -0,0 +1,24 @@
+import type { User } from '../models/user.model'
+import type { z } from 'zod'
+
+export interface UserFilter {
+  role?: string
+  search?: string
+  page?: number
+  limit?: number
+}
+
+export interface PaginatedUsers {
+  users: z.infer<typeof User>[]
+  total: number
+  page: number
+  limit: number
+}
+
+export interface UserRepositoryInterface {
+  findById: (id: string) => Promise<z.infer<typeof User> | null>
+  findAll: () => Promise<z.infer<typeof User>[]>
+  findPaginatedUsers: (filter: UserFilter) => Promise<PaginatedUsers>
+}
+
+// feat: create user model and repository - Development on 2025-05-24
