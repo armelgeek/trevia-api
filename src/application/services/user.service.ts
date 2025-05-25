8d0df4cf5f29bf5cba1053f96ec238aeb43c4daf
Hajavololona Armellin <armelgeek5@gmail.com>
2025-05-25 10:13:32 +0300

    feat: create user controller

---
+++ src/application/services/user.service.ts
@@ -0,0 +1,18 @@
+import { GetUserByIdUseCase, type GetUserByIdArgs } from '../use-cases/user/get-user-by.use-case'
+
+export class UserService {
+  private readonly getUserByIdUseCase: GetUserByIdUseCase
+
+  constructor() {
+    this.getUserByIdUseCase = new GetUserByIdUseCase()
+  }
+  getUserById = (args: GetUserByIdArgs & { currentUserId: string }) => {
+    return this.getUserByIdUseCase.run(args)
+  }
+}
+
+// feat: create user model and repository - 2025-06-21
+
+// feat: implement user CRUD operations - 2025-06-21
+
+// feat: create user controller - Development on 2025-05-25
