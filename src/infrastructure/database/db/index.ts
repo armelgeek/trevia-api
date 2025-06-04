3daa4bfceacbda6af54e8f041cb3f023b888f08f
Hajavololona Armellin <armelgeek5@gmail.com>
2025-06-04 14:03:05 +0300

    feat: create reservation system

---
+++ src/infrastructure/database/db/index.ts
@@ -0,0 +1,15 @@
+import { env } from 'node:process'
+import dotenv from 'dotenv'
+
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as schema from '../schema'
+
+dotenv.config()
+if (!env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set')
+}
+export const client = postgres(env.DATABASE_URL)
+export const db = drizzle(client, { schema })
+
+// feat: create reservation system - Development on 2025-06-04
