13751ea4a4d196641495513c6ea102975e1898a2
Hajavololona Armellin <armelgeek5@gmail.com>
2025-06-17 14:38:55 +0300

    feat: add error tracking

--- src/domain/types/activity-log.type.ts
+++ src/domain/types/activity-log.type.ts
@@ -3,3 +3,5 @@ import type { activityLogs } from '@/infrastructure/database/schema'
 export type NewActivityLog = typeof activityLogs.$inferInsert
 
 // test: implement integration tests - Development on 2025-06-12
+
+// feat: add error tracking - Development on 2025-06-17
